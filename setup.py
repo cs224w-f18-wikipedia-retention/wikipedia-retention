740fb9e5deacaf1139c98db3b0d037594b249a69
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2018-10-27 21:08:46 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Graph analysis of Wikipedia edit history',
+    author='Anthony Miyaguchi',
+)
