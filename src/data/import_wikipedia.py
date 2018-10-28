5dda62c665b5f5166f1f8cea90bd1f9f164c31be
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2018-10-28 01:08:41 -0700

    Cast into a Dataframe before saving

--- src/data/import_wikipedia.py
+++ src/data/import_wikipedia.py
@@ -1,27 +1,71 @@
 import os
 import re
 import sys
-from pyspark.sql import SparkSession
+
+from pyspark.sql import SparkSession, Row
+import pyspark.sql.functions as F
+import pyspark.sql.types as T
+
+
+wikipedia_schema = T.StructType([
+    # REVISION fields
+    T.StructField("article_id",     T.IntegerType(),    nullable=False),
+    T.StructField("rev_id",         T.IntegerType(),    nullable=False),
+    T.StructField("article_title",  T.StringType(),     nullable=False),
+    T.StructField("timestamp",      T.StringType(),     nullable=False),
+    T.StructField("username",       T.StringType(),     nullable=False),
+    T.StructField("user_id",        T.StringType(),     nullable=False),
+    # other fields
+    T.StructField("category",       T.StringType(),     nullable=True),
+    T.StructField("image",          T.StringType(),     nullable=True),
+    T.StructField("main",           T.StringType(),     nullable=True),
+    T.StructField("talk",           T.StringType(),     nullable=True),
+    T.StructField("user",           T.StringType(),     nullable=True),
+    T.StructField("user_talk",      T.StringType(),     nullable=True),
+    T.StructField("other",          T.StringType(),     nullable=True),
+    T.StructField("external",       T.StringType(),     nullable=True),
+    T.StructField("template",       T.StringType(),     nullable=True),
+    T.StructField("comment",        T.StringType(),     nullable=True),
+    T.StructField("minor",          T.BooleanType(),    nullable=False),
+    T.StructField("textdata",       T.IntegerType(),    nullable=False),
+])
+
+_cast_map = {
+    T.IntegerType: int,
+    T.BooleanType: bool,
+    T.StringType: str,
+}
+
+def default_value(item):
+    return item if len(item) > 1 else item + [None]
+
+
+def cast_values(record, schema=wikipedia_schema):
+    """Convert the values in a dictionary to the type specified in the schema."""
+    # a non-exhaustive mapping of types
+    def cast(k, v):
+        dtype = type(schema[str(k)].dataType)
+        if dtype == T.StringType:
+            return v
+        return _cast_map[dtype](v) if v else None
+
+    return {k: cast(k, v) for k, v in record.iteritems()}
 
 
 def process_edit(edit):
-    lines = edit.split('\n')
-    header_line = lines[0]
-    _revision, article_id, rev_id, article_title, timestamp, _username, user_id = header_line.split(' ')
-    minor = parse_value_line(lines[11])
-    word_count = parse_value_line(lines[12])
-    processed_arr = [article_id,user_id,minor,word_count,timestamp]
-    return ' '.join(map(str,processed_arr))
+    """Process each line in the edit history"""
 
-# Given line "NAME VALUE"
-# Gets VALUE
-# returns '' if VALUE missing
-def parse_value_line(line):
-    arr = line.split(' ')
-    if len(arr) > 1:
-        return arr[1]
-    else:
-        return ''
+    entries = [entry.split(' ', 1) for entry in edit.split('\n')]
+    # lower case keys and add default values
+    pairs = [(k.lower(), v) for k,v in map(default_value, entries)]
+
+    # each REVISION has 6 fields
+    revision = pairs[0][1]
+    revision_columns = ["article_id", "rev_id", "article_title", "timestamp", "username", "user_id"]
+    revision_pairs = zip(revision_columns, revision.split())
+
+    d = dict(revision_pairs + pairs[1:])
+    return Row(**cast_values(d))
 
 
 if __name__ == '__main__':
