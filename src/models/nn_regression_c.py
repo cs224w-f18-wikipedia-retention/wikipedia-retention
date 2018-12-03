be64996696cbdf09ea8832cde9668ef2a44b8b6e
Jonathan Hollenbeck <jonoepd@gmail.com>
2018-12-02 23:47:39 -0800

    Add code for computing community features, and new regression implementing them. Also remove models from features (meant to do with previous commit)

--- src/models/nn_regression_c.py
+++ src/models/nn_regression_c.py
@@ -1,3 +1,4 @@
+# NOTE: copy of nn_regression so I can modify without disrupting other work
 # Model for neural net regression on 2007-Q1
 import numpy as np
 import pandas as pd
@@ -6,39 +7,23 @@ import sklearn
 # load features
 uf_name = 'base_features_reg.csv'
 af_name = 'all_article_features.csv'
-rolef_name = 'enwiki-projection-user-roles-2007-1.csv'
-cf_name = 'community_norm_features.csv'
-
-# process roles
-role_df = pd.read_csv(rolef_name,delimiter='\t', header=None) # note: stripped weird header from csv
-one_hot = pd.get_dummies(role_df[1])
-# avoid conflict (use role_id as dummy later)
-role_df = role_df.rename(columns={1:'role_id'})
-one_hot = one_hot.rename(columns={0:'role0'})
-role_df = role_df.join(one_hot)
+cf_name = 'community_features.csv'
 
 user_df = pd.read_csv(uf_name, header=None)
-# mark columns that are 2007-q1
-time_str = '2007-01-01T00:00:00.000-08:00'
-time_idx = np.where(user_df[1] == '2007-01-01T00:00:00.000-08:00')[0]
 y = np.ndarray.astype(user_df.values[:,-1],int)
 user_df = user_df.drop([1,user_df.columns[-1]],axis=1) # drop time and y column
 article_df = pd.read_csv(af_name, header=None)
-community_norm_df = pd.read_csv(cf_name, header=None)
+community_df = pd.read_csv(cf_name, header=None)
+community_df = community_df.drop([1,2],axis=1)
 
 # process joined data
 ua_df = user_df.merge(article_df, on=0)
-uac_df = ua_df.merge(community_norm_df, on=0)
-X_df = uac_df.merge(role_df, how='left', on=0) 
-X = X_df.as_matrix()
-# strip by time_idx
-X = X[time_idx,:]
-y = y[time_idx]
-# set missing roles to 1 (in what used to be role_id)
-X[np.isnan(X[:,40]),40] = 1
+uac_df = ua_df.merge(community_df, how='left', on=0)
+X = uac_df.as_matrix()
 X = np.ndarray.astype(X[:,1:],float) # remove user_id
+X = np.ndarray.astype()
 X[np.isnan(X)] = 0 # clear NaNs
-# add new column for log(sum(log(textdata)))
+# add new column for log(sum(log(textdata))) TODO: fix this in base features
 lslt = np.array([np.log(X[:,5])+1]).T
 X = np.append(X, lslt, 1)
 # min-max scaling
@@ -49,9 +34,6 @@ dmin = scalar.data_min_
 dmax = scalar.data_max_
 Xnorm = scalar.transform(X)
 Xnorm = Xnorm - Xnorm.mean(axis=0)
-
-# NOTE: Above is mostly copy paste from logreg, after here is NN.
-# We should probably try to standardized our feature sets to make joining easier
 
 # setup train-test split. might want train-dev-test for final model testing
 # could also rebalance (since so many 0 examples)
@@ -86,10 +68,10 @@ def plot_yl(yl):
     plt.show()
 
 # NOTE: Below is the full 2-model (class * reg)
-alphas = [0.1, 1, 10, 100]
-nodes = [(3), (5), (7), (9), (8, 3), (10, 3), (12, 4), (14, 5)]
-#alphas = [0.1]
-#nodes = [(8,3)]
+#alphas = [0.1, 1, 10, 100]
+#nodes = [(3), (5), (7), (9), (8, 3), (10, 3), (12, 4), (14, 5)]
+alphas = [0.1]
+nodes = [(8,3)]
 params = {"alpha" : alphas, "hidden_layer_sizes" : nodes}
 theta_idx = yl > 0
 yl_theta = yl[theta_idx]
